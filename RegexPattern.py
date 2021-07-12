79de1b3ca40b0ad063ffd9217a4cedb2ff3f0074
shivammathur4 <35236428+shivammathur4@users.noreply.github.com>
2021-07-12 10:06:35 +0530

    [Shivam] Add : Files for UC7 User Registration Program

--- RegexPattern.py
+++ RegexPattern.py
@@ -3,4 +3,4 @@ class RegexPattern:
     NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}"
     EMAIL_PATTERN = "^[a-zA-Z]+(\.[a-z-]+)@?[A-Za-z0-9-]+(\.[a-z]+)*(\.[a-z]{2,})$"
     PHONE_PATTERN = "^[\+][9?][1?][\-\s?][6-9]{1}[\d]{9}$"
-    PASSWORD_PATTERN = "^(?=.*[A-Z])[A-Za-z]{8,}$"
+    PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}$"
